fc5a405fa6af22d25ca81e16c6dc7f2b722f81fb
Talijudith <judithtali59@gmail.com>
2024-09-30 05:19:26 -0700

    Course Work

--- Form1.vb
+++ Form1.vb
@@ -1,19 +1,42 @@
 ﻿Public Class Form1
-    ' Event handler for button click
-    Private Sub btnCountDown_Click(sender As Object, e As EventArgs) Handles btnCountDown.Click
-        ' Get the number from the textbox
-        Dim number As Integer
 
-        If Integer.TryParse(txtNumber.Text, number) Then
-            lblCountdownResult.Text = ""  ' Clear previous result
+    Private Sub btnevaluate_Click(sender As Object, e As EventArgs) Handles btnevaluate.Click
+        Dim marks As Integer
 
-            ' While loop to count down from the given number to zero
-            While number >= 0
-                lblCountdownResult.Text &= number.ToString() & " "  ' Append each number to the label with a space
-                number -= 1
-            End While
-        Else
-            lblCountdownResult.Text = "Please enter a valid number."  ' Show error if input is not a valid number
+        'input validation for marks to ensure numeric input'
+        If Not Integer.TryParse(txtentermarks.Text, marks) Then
+            MessageBox.Show("please enter a valid numeric avalue for marks.")
+            Exit Sub
         End If
+        ' check for marks exceed 100'
+        If marks > 100 Then
+            MessageBox.Show("wrong entry, please re-enter the mark")
+            Exit Sub
+        End If
+        ' using select case to determine the grade based on marks'
+        Select Case marks
+            Case 90 To 100
+                txtgrade.Text = "Excellent"
+            Case 80 To 89
+                txtgrade.Text = "Very Good"
+            Case 70 To 79
+                txtgrade.Text = "Good"
+            Case 60 To 69
+                txtgrade.Text = "Medium"
+            Case 50 To 59
+                txtgrade.Text = "Pass"
+            Case 0 To 49
+                txtgrade.Text = "Fail"
+            Case Else
+                txtgrade.Text = "Invalid Marks"
+        End Select
+    End Sub
+
+    Private Sub txtentermarks_TextChanged(sender As Object, e As EventArgs) Handles txtentermarks.TextChanged
+1
+    End Sub
+
+    Private Sub txtgrade_TextChanged(sender As Object, e As EventArgs) Handles txtgrade.TextChanged
+
     End Sub
 End Class
